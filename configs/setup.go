d21662d6b64ab16de238bfe091554c2239f31723
liyizhou <liyizhou@isv-tech.com>
2024-04-23 20:57:29 +0800

    Implement Singleton pattern for database connection

--- configs/setup.go
+++ configs/setup.go
@@ -2,20 +2,28 @@ package configs
 
 import (
 	"log"
+	"sync"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
+var (
+	db   *gorm.DB
+	once sync.Once
+)
+
 // ConnectMySQL connects to MySQL and returns a pointer to the database.
 func ConnectMySQL() *gorm.DB {
-	dsn := EnvMySQLURI()
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	log.Println("Connected to MySQLDB!")
+	once.Do(func() {
+		dsn := EnvMySQLURI()
+		var err error
+		db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println("Connected to MySQLDB!")
+	})
 	return db
 }
 
@@ -26,6 +34,6 @@ var DB = ConnectMySQL()
 func AutoMigrate(db *gorm.DB, model interface{}) {
 	err := db.AutoMigrate(&model)
 	if err != nil {
-		return
+		log.Fatal(err)
 	}
 }
