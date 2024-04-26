4548cc9bb08374e175d57a792e9fdeb7877013fc
liyizhou <liyizhou@isv-tech.com>
2024-04-26 14:45:18 +0800

    Refactored todo routes to be under /todos for consistency

--- routes/todo_route.go
+++ routes/todo_route.go
@@ -1,8 +1,9 @@
 package routes
 
 import (
-	"github.com/gin-gonic/gin"
 	"todo-backend/controllers"
+
+	"github.com/gin-gonic/gin"
 )
 
 var todoController = &controllers.TodoController{}
@@ -10,8 +11,8 @@ var todoController = &controllers.TodoController{}
 // TodoRoute represents todo-related routes
 func TodoRoute(router *gin.Engine) {
 	router.GET("/todos", todoController.GetTodos)
-	router.GET("/todo/:id", todoController.GetTodoByID)
-	router.POST("/todo", todoController.CreateTodo)
-	router.PUT("/todo/:id", todoController.UpdateTodo)
-	router.DELETE("/todo/:id", todoController.DeleteTodoByID)
+	router.POST("/todos", todoController.CreateTodo)
+	router.GET("/todos/:id", todoController.GetTodoByID)
+	router.PUT("/todos/:id", todoController.UpdateTodo)
+	router.DELETE("/todos/:id", todoController.DeleteTodoByID)
 }
