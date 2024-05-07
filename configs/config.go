6ef3e78ee097e04a5603ef37f97b3722698b4c01
liyizhou <liyizhou@isv-tech.com>
2024-05-07 20:13:45 +0800

    Load config info to cfg struct

--- configs/config.go
+++ configs/config.go
@@ -1,19 +1,53 @@
 package configs
 
 import (
-	"log"
-
 	"github.com/spf13/viper"
 )
 
+type Config struct {
+	App   `mapstructure:"app"`
+	Http  `mapstructure:"http"`
+	Log   `mapstructure:"log"`
+	MySQL `mapstructure:"mysql"`
+}
+
+type App struct {
+	Name    string `mapstructure:"name"`
+	Version string `mapstructure:"version"`
+}
+
+type Http struct {
+	Port string `mapstructure:"port"`
+}
+
+type Log struct {
+	Level string `mapstructure:"level"`
+}
+
+type MySQL struct {
+	Host     string `mapstructure:"host"`
+	Port     string `mapstructure:"port"`
+	Username string `mapstructure:"username"`
+	Password string `mapstructure:"password"`
+	Database string `mapstructure:"database"`
+}
+
 // LoadConfig reads the configuration file.
-func LoadConfig() {
+func LoadConfig(path string) (*Config, error) {
+	viper.AddConfigPath(path)
 	viper.SetConfigName("config")
 	viper.SetConfigType("yaml")
-	viper.AddConfigPath("configs/")
 
-	err := viper.ReadInConfig()
-	if err != nil {
-		log.Fatalf("Error reading config file, %s", err)
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, err
 	}
+
+	var cfg Config
+	if err := viper.Unmarshal(&cfg); err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
 }
