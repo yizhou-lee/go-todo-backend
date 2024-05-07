e74c23483fbf0b9ea57543fcb84fc2a2959c4841
liyizhou <liyizhou@isv-tech.com>
2024-05-07 20:11:00 +0800

    Move main.go to cmd/app

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -4,31 +4,35 @@ import (
 	"log"
 	"net/http"
 	"todo-backend/configs"
-	"todo-backend/models"
-	"todo-backend/routes"
+	"todo-backend/internal/models"
+	"todo-backend/internal/routes"
+	"todo-backend/pkg/mysql"
 	"todo-backend/utils"
-
-	"todo-backend/pkg/sql"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	// Read configuration
-	configs.LoadConfig()
+	path := "./configs"
+	cfg, err := configs.LoadConfig(path)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	// Connect to database and migrate the model
-	db := sql.ConnectMySQL()
-	sql.AutoMigrate(db, &models.Todo{})
+	db := mysql.ConnectMySQL(cfg)
+	mysql.AutoMigrate(db, &models.Todo{})
 
 	router := gin.Default()
+
 	// Routes
 	routes.TodoRoute(router)
 	router.NoRoute(func(c *gin.Context) {
 		c.JSON(http.StatusNotFound, utils.ErrorResponse("Page not found", nil))
 	})
 
-	err := router.Run("localhost:8080")
+	err = router.Run("localhost:8080")
 	if err != nil {
 		log.Fatal(err)
 	}
